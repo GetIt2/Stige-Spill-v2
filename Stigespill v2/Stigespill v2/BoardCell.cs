60fef1f17d4a16c02904cbfbc217bccb811f2492
Kristoffer Kvamme <33118819+KrisKvam@users.noreply.github.com>
2018-04-19 11:02:39 +0200

    Add files via upload

---
+++ Stigespill v2/Stigespill v2/BoardCell.cs
@@ -0,0 +1,62 @@
+﻿namespace Stigespill_v2
+{
+    class BoardCell
+    {
+        public bool Up { get; private set; }
+        public bool Down { get; private set; }
+        public bool Left { get; private set; }
+        public bool Right { get; private set; }
+
+        public void AddHorizontal()
+        {
+            Left = Right = true;
+        }
+
+        public void AddVertical()
+        {
+            Up = Down = true;
+        }
+
+        public void AddLowerLeftCorner()
+        {
+            Up = Right = true;
+        }
+
+        public void AddUpperLeftCorner()
+        {
+            Down = Right = true;
+        }
+
+        public void AddUpperRightCorner()
+        {
+            Down = Left = true;
+        }
+
+        public void AddLowerRightCorner()
+        {
+            Up = Left = true;
+        }
+
+        public char GetCharacter()
+        {
+            if (Up && Down && Left && Right) return '┼';
+            if (Up && Down && Left && !Right) return '┤';
+            if (Up && Down && !Left && Right) return '├';
+            if (Up && Down && !Left && !Right) return '│';
+            if (Up && !Down && Left && Right) return '┴';
+            if (Up && !Down && Left && !Right) return '┘';
+            if (Up && !Down && !Left && Right) return '└';
+            if (Up && !Down && !Left && !Right) return '╵';
+
+            if (!Up && Down && Left && Right) return '┬';
+            if (!Up && Down && Left && !Right) return '┐';
+            if (!Up && Down && !Left && Right) return '┌';
+            if (!Up && Down && !Left && !Right) return '╷';
+            if (!Up && !Down && Left && Right) return '─';
+            if (!Up && !Down && Left && !Right) return '╴';
+            if (!Up && !Down && !Left && Right) return '╶';
+
+            return ' ';
+        }
+    }
+}
---
+++ Stigespill v2/Stigespill v2/BoardRow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Stigespill_v2
+{
+    class BoardRow
+    {
+        private BoardCell[] _cells;
+
+        public BoardRow(int width)
+        {
+            _cells = new BoardCell[width];
+            for (var i = 0; i < _cells.Length; i++)
+            {
+                _cells[i] = new BoardCell();
+            }
+        }
+
+        public void AddTopRow(int startX, int width)
+        {
+            var endX = startX + width - 1;
+            _cells[startX].AddUpperLeftCorner();
+            _cells[endX].AddUpperRightCorner();
+            for (var colId = startX + 1; colId < endX; colId++)
+            {
+                _cells[colId].AddHorizontal();
+            }
+        }
+
+        public void AddBottomRow(int startX, int width)
+        {
+            var endX = startX + width - 1;
+            _cells[startX].AddLowerLeftCorner();
+            _cells[endX].AddLowerRightCorner();
+            for (var colId = startX + 1; colId < endX; colId++)
+            {
+                _cells[colId].AddHorizontal();
+            }
+        }
+
+        public void AddMiddleRow(int startX, int width)
+        {
+            _cells[startX].AddVertical();
+            _cells[startX + width - 1].AddVertical();
+        }
+
+        public void Show()
+        {
+            var array = _cells.Select(c => c.GetCharacter()).ToArray();
+            Console.WriteLine(array);
+        }
+    }
+}
